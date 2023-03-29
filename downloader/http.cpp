0e95b378f03012e7705f42f14bf9b104970c3590
dengkaitian <2286859221@qq.com>
2023-03-29 09:25:39 -0700

    feature - http 添加字段及字段函数

--- downloader/http.cpp
+++ downloader/http.cpp
@@ -4,7 +4,47 @@ namespace downloader {
 
 namespace proto {
 
-Http::~Http() = default;
+struct Http::ContentType {
+    std::string type;
+    std::string parameter;
+};
+
+Http::Http() : content_type_(new ContentType),
+               content_length_(0)
+{}
+
+Http::~Http() {
+    if (content_type_ != nullptr) delete content_type_;
+}
+
+void Http::SetContentEncoding(std::string encoding) {
+    content_encoding_ = encoding;
+}
+
+std::string Http::GetContentEncoding() const {
+    return content_encoding_;
+}
+
+void Http::SetContentType(std::string type, std::string param) {
+    content_type_->type = type;
+    content_type_->parameter = param;
+}
+
+std::string Http::GetContentType() const {
+    return content_type_->type;
+}
+
+std::string Http::GetContentTypeParameter() const {
+    return content_type_->parameter;
+}
+
+void Http::SetContentLength(uint64_t len) {
+    content_length_ = len;
+}
+
+uint64_t Http::GetContentLength() const {
+    return content_length_;
+}
 
 }   // namespace proto
 
--- include/downloader/http.h
+++ include/downloader/http.h
@@ -1,20 +1,41 @@
 #ifndef _HTTP_H_    // NOLINT
 #define _HTTP_H_    // NOLINT
 
+#include <cstdint>
+#include <string>
+
 #include "downloader/proto.h"
 
 namespace downloader {
 
 namespace proto {
 
+// TODO(xxx) : 完善 http 字段
 class Http : public ProtoBase {
  public:
-    Http() = default;
+    Http();
 
     Http(const Http&) = delete;
     Http& operator=(const Http&) = delete;
 
     ~Http() override;
+
+    void SetContentEncoding(std::string encoding);
+    std::string GetContentEncoding() const;
+
+    void SetContentType(std::string type, std::string param);
+    std::string GetContentType() const;
+    std::string GetContentTypeParameter() const;
+
+    void SetContentLength(uint64_t len);
+    uint64_t GetContentLength() const;
+
+ private:
+    struct ContentType;
+
+    std::string content_encoding_;
+    ContentType* content_type_;
+    uint64_t content_length_;
 };
 
 }   // namespace proto
